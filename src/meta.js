70ee0596cd900609edba8886c6e17f583bb90879
Petra Jaros <peeja@peeja.com>
2022-04-16 10:52:40 -0400

    Remove rogue file from Rollup days

--- src/meta.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name        My Script
-// @namespace   Violentmonkey Scripts
-// @description This is a userscript.
-// @match       *://*/*
-// @version     process.env.VERSION
-// @author      process.env.AUTHOR
-// ==/UserScript==
